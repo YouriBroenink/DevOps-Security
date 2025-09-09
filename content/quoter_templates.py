4914309a533548d76c240213590cc9c32e6a1a7e
YouriBroenink <516909@student.saxion.nl>
2025-09-09 17:22:31 +0200

    Testen

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen - Youri Broenink</label>
+<label class="link" for="signinCheckbox">Inloggen - Youri Broenink 516909 test</label>
 """
 
     return f"""<!DOCTYPE html>
