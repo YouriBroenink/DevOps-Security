b5027b6b73a73fe689c4a025f846f7507f7cc155
YouriBroenink <516909@student.saxion.nl>
2025-09-23 16:09:42 +0200

    Test Postman

--- content/quoter_templates.py
+++ content/quoter_templates.py
@@ -60,7 +60,7 @@ def page(content,user_id,title,error=None):
 """
     else:
         links = f"""
-<label class="link" for="signinCheckbox">Inloggen - Youri Broenink - 516909 - Test</label>
+<label class="link" for="signinCheckbox">Inloggen - Youri Broenink - 516909</label>
 """
 
     return f"""<!DOCTYPE html>
